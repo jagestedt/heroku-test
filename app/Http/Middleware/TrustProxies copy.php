6b3bdb2a863e2deb6f9001818122db381890a194
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-02-01 11:53:02 +0100

    update review app heroku

--- app/Http/Middleware/TrustProxies copy.php
+++ app/Http/Middleware/TrustProxies copy.php
@@ -12,13 +12,12 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = '*'; //heroku added = '*'
+    protected $proxies;
+
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    // added heroku
-    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
-    //protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
 }
--- config/app.php
+++ config/app.php
@@ -52,7 +52,10 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    /*matching heroku*/
+    'url' => env('APP_URL', env('HEROKU_APP_NAME') ? 'https://' . env('HEROKU_APP_NAME') . '.herokuapp.com' : 'http://localhost'),
+
+    //'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -118,8 +121,9 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-
-    'key' => env('APP_KEY'),
+    /*matching heroku*/
+    'key' => strpos(env('APP_KEY'), 'base64:') !== false ? env('APP_KEY') : substr(env('APP_KEY'), 0, 32),
+    //'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
