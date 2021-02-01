edbbf1311bddefeb83eddbf9da938833eb14a6c2
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-02-01 18:18:24 +0100

    update database.php

--- config/database.php
+++ config/database.php
@@ -17,8 +17,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION'),
-    // 'default' => env('DB_CONNECTION', 'pgsql'),
+    // 'default' => env('DB_CONNECTION'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
